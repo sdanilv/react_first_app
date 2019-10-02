0b0fcc4323249246d5e064177619ff5efe525b01
DivinusIocus <DivinusIocus@MDIVINUSIOCUS>
2019-10-02 15:00:26 +0300

    Start

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React  from "react"
+
+const Header = () =>{
+    return(
+<header>
+    <img src = 'https://s1.logaster.com/static/v3/img/products/logo.png' alt='abc' />
+    Header
+</header>
+    );
+}
+
+export default Header;

7c7e4bed224931d132d63d7003cef59c60f121e3
DivinusIocus <DivinusIocus@MDIVINUSIOCUS>
2019-10-23 14:21:43 +0300

    coment

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-const Auth = (props) => {
+import React from "react";
+import {} from "redux-form";
+const Auth = props => {
   return (
     <center>
       <h2>Authorization</h2>
-      <form action="auth" method="post">
-        <div>Login</div>
-        <div><input type="text" name="" id="" /></div>
-        <div>Password</div>
-        <div><input type="password" /></div>
+      <form action='auth' method='post'>
+        <div>
+          <input placeholder='Login' type='text' name='' id='' />
+        </div>
+        <div>
+          <input placeholder='Password' type='password' />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
       </form>
     </center>
-  )
-}
+  );
+};
 
 export default Auth;

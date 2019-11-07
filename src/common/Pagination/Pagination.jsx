5ee6f90e17347afd6018fad6c57c4f6eab6c756f
DivinusIocus <DivinusIocus@MDIVINUSIOCUS>
2019-11-07 14:23:32 +0200

    Pagination

--- src/common/Pagination/Pagination.jsx
+++ src/common/Pagination/Pagination.jsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, {memo, useState} from "react";
 import style from "./Pagination.module.css";
+let activeKit = 1;
+const Pagination = memo(props => {
+    const [kit, setKit] = useState(1);
+    const {currentPage, numbersPage, onPageClick, countOfVisiblePages = 5} = props;
 
-const Pagination = props => {
-  let { currentPage, numbersPage } = props;
-  let arrayOfPages = [];
+    // let countOfVisiblePages = 5;
+    const lastKit = Math.ceil(numbersPage/countOfVisiblePages);
+    const lastPage = numbersPage -1;
+    let start = (kit - 1) * countOfVisiblePages + 1;
+    let end = kit * countOfVisiblePages;
 
-  for (let i = 1; i < numbersPage; i++) {
-    arrayOfPages.push(i);
-  }
+    let arrayOfPages = [];
 
-  let button = p => {
-    return (
-      <button
-        className={currentPage === p ? style.selectedPage : null}
-        key={p}
-        onClick={e => props.onPageClick(p)}>
-        {p}
-      </button>
-    );
-  };
-
-  let pages = arrayOfPages.map(p => {
-    if ((p >= 1 && p <= 5) || p === arrayOfPages.length) {
-      return button(p);
+    for (let i = 1; i < numbersPage; i++) {
+        arrayOfPages.push(i);
     }
-    if (p === arrayOfPages.length - 1) return <button key={p}>...</button>;
-    return "";
-  });
 
-  return <div className={style.pagination}>{pages}</div>;
-};
+    let button = p => {
+        return (
+            <button
+                className={currentPage === p ? style.selectedPage : null}
+                key={p}
+                onClick={e => {
+                  // if(p===1) {activeKit= 1}
+                  // if(p===lastPage)
+                  //  { activeKit = lastKit;}
+                  onPageClick(p);}}>
+                {p}
+            </button>
+        );
+    };
+
+    let pages = (start, end) => arrayOfPages.map(p => {
+
+        if ((p >= start && p <= end) || p === arrayOfPages.length || p === 1) {
+            return button(p);
+        }
+        if (start !== 1 && p === 2) return <button onClick={() => setKit(kit - 1)} key={p}>...</button>;
+        if (p === arrayOfPages.length - 1 && end !== arrayOfPages.length)
+            return <button onClick={() => setKit(kit + 1)} key={p}>...</button>;
+        return "";
+    });
+
+    return <div className={style.pagination}>{pages(start, end)}</div>;
+});
 
 export default Pagination;

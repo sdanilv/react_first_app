267fcb483ad43ef1a3691f83a5a4662dd4339c4f
DivinusIocus <DivinusIocus@MDIVINUSIOCUS>
2019-11-19 09:01:59 +0200

    github pages

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
   }
 
   render() {
-    alert("2");
+    alert("3");
     if (this.props.isLocked) return <PageLoader />;
     return (
       <BrowserRouter basename={process.env.PUBLIC_URL}>
--- src/api/api.js
+++ src/api/api.js
@@ -6,7 +6,7 @@ const axiosInstance = axios.create({
     withCredentials: true,
     headers: {
         "API-KEY": "ade57208-42e1-4033-bb19-07633193cdde",
-        // "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "*",
         // "Access-Control-Allow-Credentials": "true",
         // "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         // "Access-Control-Allow-Headers": "Origin, Content-Type, Accept"
